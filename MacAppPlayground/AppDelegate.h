d211f76f2674b4a8441e6b7096fb77221d0f6fe4
Jakub Suder <jakub.suder@gmail.com>
2013-02-08 19:01:56 +0100

    Initial Commit

---
+++ MacAppPlayground/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacAppPlayground
+//
+//  Created by Jakub Suder on 08.02.2013.
+//  Copyright (c) 2013 Jakub Suder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacAppPlayground/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MacAppPlayground
+//
+//  Created by Jakub Suder on 08.02.2013.
+//  Copyright (c) 2013 Jakub Suder. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
